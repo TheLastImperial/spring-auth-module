60e33d4b81a5af0bd38380c34ffa142f7c5c1e51
TheLastImperial <17633123+TheLastImperial@users.noreply.github.com>
2026-08-23 01:49:37 -0700

    Added rememberme service and implementation.

--- src/auth/src/main/java/com/thelastimperial/auth/auth/autoconfiguration/AuthAutoConfiguration.java
+++ src/auth/src/main/java/com/thelastimperial/auth/auth/autoconfiguration/AuthAutoConfiguration.java
@@ -1,12 +1,20 @@
 package com.thelastimperial.auth.auth.autoconfiguration;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.RememberMeServices;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenBasedRememberMeServices;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
 import com.thelastimperial.auth.auth.services.ActivationService;
 import com.thelastimperial.auth.auth.services.AuditService;
@@ -45,7 +53,28 @@ public class AuthAutoConfiguration {
 
     @Bean
     @ConditionalOnMissingBean
-    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) {
+    public RememberMeServices rememberMeServices(
+        @Value("${com.thelastimperial.auth.rememberme.key}") String key,
+        UserDetailsService userDetailsService, PersistentTokenRepository persistentTokenRepository
+    ){
+        return new PersistentTokenBasedRememberMeServices(
+            key, userDetailsService, persistentTokenRepository
+        );
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public JdbcTokenRepositoryImpl getJdbcTokenRepositoryImpl(DataSource dataSource){
+        JdbcTokenRepositoryImpl tokenRepository = new JdbcTokenRepositoryImpl();
+        tokenRepository.setDataSource(dataSource);
+        return tokenRepository;
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http,
+        RememberMeServices rememberMeServices
+    ) {
         http
         .authorizeHttpRequests(auth -> auth
             .requestMatchers("/").hasRole("USER")
@@ -56,6 +85,10 @@ public class AuthAutoConfiguration {
             .failureUrl("/auth/login?error=true")
             .defaultSuccessUrl("/", true)
             .permitAll()
+        )
+        .rememberMe(rememberme -> rememberme
+            .rememberMeServices(rememberMeServices)
+            .rememberMeParameter("remember-me")
         )
         .logout(logout -> logout
             .logoutUrl("/auth/logout")
