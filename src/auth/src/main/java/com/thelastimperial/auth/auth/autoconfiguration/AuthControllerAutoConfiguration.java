9ad098901da35cd2f9ae62520e21a3aa710dddda
TheLastImperial <17633123+TheLastImperial@users.noreply.github.com>
2026-08-24 21:06:17 -0700

    Beans Controllers AutoConfiguration created. Updated POM version

---
+++ src/auth/src/main/java/com/thelastimperial/auth/auth/autoconfiguration/AuthControllerAutoConfiguration.java
@@ -0,0 +1,50 @@
+package com.thelastimperial.auth.auth.autoconfiguration;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+
+import com.thelastimperial.auth.auth.controllers.ActivationController;
+import com.thelastimperial.auth.auth.controllers.LoginController;
+import com.thelastimperial.auth.auth.controllers.NewPasswordController;
+import com.thelastimperial.auth.auth.controllers.RecoveryController;
+import com.thelastimperial.auth.auth.controllers.RegisterController;
+import com.thelastimperial.auth.auth.services.ActivationService;
+import com.thelastimperial.auth.auth.services.NewPasswordService;
+import com.thelastimperial.auth.auth.services.RecoveryService;
+import com.thelastimperial.auth.auth.services.RegisterService;
+
+@AutoConfiguration
+@AutoConfigureAfter(AuthAutoConfiguration.class)
+public class AuthControllerAutoConfiguration {
+    @Bean
+    @ConditionalOnMissingBean
+    public ActivationController activationController(ActivationService activationService) {
+        return new ActivationController(activationService);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public LoginController loginController() {
+        return new LoginController();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public NewPasswordController newPasswordController(NewPasswordService newPasswordService) {
+        return new NewPasswordController(newPasswordService);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public RecoveryController recoveryController(RecoveryService recoveryService){
+        return new RecoveryController(recoveryService);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public RegisterController registerController(RegisterService registerService) {
+        return new RegisterController(registerService);
+    }
+}
