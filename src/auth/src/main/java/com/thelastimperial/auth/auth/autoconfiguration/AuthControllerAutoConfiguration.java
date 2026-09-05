1183276a4d580cbc9ce78a7f9c297edcf818c3c8
TheLastImperial <17633123+TheLastImperial@users.noreply.github.com>
2026-09-04 21:03:43 -0700

    Conditional Bean names to controllers.

--- src/auth/src/main/java/com/thelastimperial/auth/auth/autoconfiguration/AuthControllerAutoConfiguration.java
+++ src/auth/src/main/java/com/thelastimperial/auth/auth/autoconfiguration/AuthControllerAutoConfiguration.java
@@ -20,37 +20,37 @@ import com.thelastimperial.auth.auth.services.RegisterService;
 @AutoConfigureAfter(AuthAutoConfiguration.class)
 public class AuthControllerAutoConfiguration {
     @Bean
-    @ConditionalOnMissingBean
+    @ConditionalOnMissingBean(name="activationController")
     public ActivationController activationController(ActivationService activationService) {
         return new ActivationController(activationService);
     }
 
     @Bean
-    @ConditionalOnMissingBean
+    @ConditionalOnMissingBean(name="loginController")
     public LoginController loginController() {
         return new LoginController();
     }
 
     @Bean
-    @ConditionalOnMissingBean
+    @ConditionalOnMissingBean(name="newPasswordController")
     public NewPasswordController newPasswordController(NewPasswordService newPasswordService) {
         return new NewPasswordController(newPasswordService);
     }
 
     @Bean
-    @ConditionalOnMissingBean
+    @ConditionalOnMissingBean(name="recoveryController")
     public RecoveryController recoveryController(RecoveryService recoveryService){
         return new RecoveryController(recoveryService);
     }
 
     @Bean
-    @ConditionalOnMissingBean
+    @ConditionalOnMissingBean(name="registerController")
     public RegisterController registerController(RegisterService registerService) {
         return new RegisterController(registerService);
     }
 
     @Bean
-    @ConditionalOnMissingBean
+    @ConditionalOnMissingBean(name="homeController")
     public HomeController homeController() {
         return new HomeController();
     }
